8d95a83b5f55c4432009795c1eea8d68185e0878
Air Erik <airerik@yandex.ru>
2024-01-17 13:05:56 +0300

    Функция передает в DB классы списком

--- 03_init_database/class_creator.py
+++ 03_init_database/class_creator.py
@@ -16,18 +16,16 @@ with open(pth_dataset) as yaml_file:
 class_names = yaml_read_data['names']
 class_count = yaml_read_data['nc']
 
-print(class_names)
-
 query = sql.SQL('''
-    INSERT INTO {table_val} (class) VALUES ({len_val});
+    INSERT INTO {table_val} (class) VALUES ({len_val})
 ''').format(
     table_val=sql.Identifier('workflow', 'classes'),
     len_val=sql.SQL('), (').join(sql.Placeholder() * len(class_names))
 )
 
-
 with psycopg.connect('dbname=ai_project user=ayrapetov_es \
 password=1111') as conn:
+    print(query.as_string(conn))
     conn.execute(
         query, class_names
     )
