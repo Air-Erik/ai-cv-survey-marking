19b2022be3601b1252d1de29489a6546964ad6a8
Air Erik <airerik@yandex.ru>
2024-02-01 18:05:25 +0300

    Попытки реализовать код для удаления дубликатов

--- 02_mark/test.py
+++ 02_mark/test.py
@@ -5,9 +5,17 @@ schema_name_in_db = 'workflow'
 mark_table_name_in_db = 'raw_mark_data'
 
 query_unique = sql.SQL('''
-    SELECT DISTINCT
-        image_name
-    FROM {table_raw_mark}
+DELETE FROM {table_raw_mark}
+WHERE (x_1, y_1, x_2, y_2, image_name) IN (
+    SELECT x_1, y_1, x_2, y_2, image_name
+    FROM (
+        SELECT
+            x_1, y_1, x_2, y_2, image_name,
+            ROW_NUMBER() OVER (PARTITION BY x_1, y_1, x_2, y_2, image_name ORDER BY (SELECT NULL)) AS row_num
+        FROM {table_raw_mark}
+    ) AS subquery
+    WHERE row_num > 1
+);
 ''').format(
     table_raw_mark=sql.Identifier(schema_name_in_db, mark_table_name_in_db)
 )
