d664d241097d5fa16115043f788e0691ac3a1496
Air Erik <airerik@yandex.ru>
2024-02-07 13:24:10 +0300

    Создание функци и тригеров в базе данных

--- 03_init_database/triges_create.py
+++ 03_init_database/triges_create.py
@@ -1,13 +1,21 @@
+import psycopg
+from psycopg import sql
+
+
+# Название схемы и таблиц
+schema_name_in_db = 'workflow'
+raw_mark_table_name_in_db = 'raw_mark_data'
+mark_table_name_in_db = 'mark_data'
 
 # SQL запрос на создание функции проверки строки на уникальность по  5 столбцам
-'''
+query_befor_insert = sql.SQL('''
 CREATE OR REPLACE FUNCTION before_insert()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Проверяем наличие дубликатов перед вставкой
     IF EXISTS (
         SELECT 1
-        FROM workflow.raw_mark_data
+        FROM {table}
         WHERE x_1 = NEW.x_1
             AND y_1 = NEW.y_1
             AND x_2 = NEW.x_2
@@ -22,12 +30,17 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-'''
+''').format(table=sql.Identifier(schema_name_in_db, raw_mark_table_name_in_db))
 
 # SQL запрос на создание тригера перед вставкой для проверки уникальности
-'''
+qurey_triger_before_insert = sql.SQL('''
 CREATE TRIGGER check_unique_before_insert
-BEFORE INSERT ON workflow.raw_mark_data
+BEFORE INSERT ON {table}
 FOR EACH ROW
 EXECUTE FUNCTION before_insert();
-'''
+''').format(table=sql.Identifier(schema_name_in_db, raw_mark_table_name_in_db))
+
+with psycopg.connect('dbname=ai_project user=API_write_data \
+password=1111') as conn:
+    conn.execute(query_befor_insert)
+    conn.execute(qurey_triger_before_insert)
