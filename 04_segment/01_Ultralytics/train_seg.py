901ca3fbae0b9819d53e7c0d41c5189f653b7942
Air Erik <airerik@yandex.ru>
2024-03-04 16:55:39 +0300

    Добавил подключение к clearML

--- 04_segment/01_Ultralytics/train_seg.py
+++ 04_segment/01_Ultralytics/train_seg.py
@@ -1,3 +1,4 @@
+from clearml import Task
 from ultralytics import YOLO
 import torch
 
@@ -10,14 +11,26 @@ def main():
     print(torch.cuda.device(0))
     print(torch.cuda.get_device_name(0))
 
+    # ClearML; Создание объекта задачи для clearml, описывает проект и
+    # название текущей сессии
+    task = Task.init(
+        project_name="AutoCAD_segment",
+        task_name="pipe_4cls_pipe _only_balance.v1"
+        )
+
+    # ClearML; Определение модели на которой будет происходить обучение
+    model_variant = "yolov8l"
+    task.set_parameter("model_variant", model_variant)
+
     model = YOLO('yolov8l-seg.pt')
 
-    args = dict(data='datasets/Segment_AI/data.yaml',
+    args = dict(data='datasets/pipe_4cls_pipe _only_balance.v1/data.yaml',
                 epochs=100,
                 imgsz=640,
                 freeze=8,
                 patience=20,
                 )
+    task.connect(args)
 
     model.train(**args)
 
